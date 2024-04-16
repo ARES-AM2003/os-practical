e958431b7923ad50bf99e6434a3d1e7959432cd9
ares <rochaksulu2002@gmail.com>
2024-04-16 15:47:32 +0545

    os

---
+++ practical/fcfs_process.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+
+// Structure to represent a process
+struct Process {
+    int pid; // Process ID
+    int burst_time; // Burst time
+    int arrival_time; // Arrival time
+};
+
+// Function to perform FCFS scheduling
+void FCFS(struct Process proc[], int n) {
+    // Array to store completion time of each process
+    int completion_time[n];
+    completion_time[0] = proc[0].burst_time;
+
+    // Calculating completion time of each process
+    for (int i = 1; i < n; i++) {
+        completion_time[i] = completion_time[i-1] + proc[i].burst_time;
+    }
+
+    // Array to store waiting time of each process
+    int waiting_time[n];
+    waiting_time[0] = 0;
+
+    // Calculating waiting time of each process
+    for (int i = 1; i < n; i++) {
+        waiting_time[i] = completion_time[i-1] - proc[i].arrival_time;
+    }
+
+    // Printing the order of execution
+    printf("Order of execution: ");
+    for (int i = 0; i < n; i++) {
+        printf("P%d ", proc[i].pid);
+    }
+    printf("\n");
+
+    // Printing waiting time and turnaround time for each process
+    printf("\nProcess\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t\t%d\t\t%d\t\t%d\n", proc[i].pid, proc[i].arrival_time, proc[i].burst_time, waiting_time[i], completion_time[i] - proc[i].arrival_time);
+    }
+
+    // Calculating average waiting time and average turnaround time
+    float avg_waiting_time = 0;
+    float avg_turnaround_time = 0;
+    for (int i = 0; i < n; i++) {
+        avg_waiting_time += waiting_time[i];
+        avg_turnaround_time += completion_time[i] - proc[i].arrival_time;
+    }
+    avg_waiting_time /= n;
+    avg_turnaround_time /= n;
+
+    printf("\nAverage Waiting Time: %.2f\n", avg_waiting_time);
+    printf("Average Turnaround Time: %.2f\n", avg_turnaround_time);
+}
+
+int main() {
+    int n;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    // Array to store processes
+    struct Process proc[n];
+
+    // Input arrival time and burst time for each process
+    for (int i = 0; i < n; i++) {
+        printf("Enter arrival time for process P%d: ", i+1);
+        scanf("%d", &proc[i].arrival_time);
+        printf("Enter burst time for process P%d: ", i+1);
+        scanf("%d", &proc[i].burst_time);
+        proc[i].pid = i + 1;
+    }
+
+    // Perform FCFS scheduling
+    FCFS(proc, n);
+
+    return 0;
+}
